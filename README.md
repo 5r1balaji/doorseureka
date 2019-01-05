01827c14183f02081a90b241466249d156339acd
Sribalaji <sreebalaji.vasanth@gmail.com>
2019-01-05 14:53:29 +0530

    Initial Commit for Eureka Server

---
+++ README.md
@@ -0,0 +1,7 @@
+################### DOORS Eureka Server ########################
+This Application works as the Discovery Server for all the microservices registered with it .
+Currently its deployed in port 8761
+
+Tools used:
+Spring boot 2.1.1
+java 1.8
---
+++ src/test/java/com/agam/doors/DoorsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.agam.doors;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DoorsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
